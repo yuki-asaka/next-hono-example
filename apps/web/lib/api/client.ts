c5647969cf864543ca469c334748ad17fed38a76
Yuki Asaka <yuki-asaka@users.noreply.github.com>
2025-11-06 23:42:57 +0900

    fix(web): lint

--- apps/web/lib/api/client.ts
+++ apps/web/lib/api/client.ts
@@ -1,4 +1,28 @@
 import type { AppType } from '@repo/app'
 import { hc } from 'hono/client'
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!)
+const customFetch = async (
+  input: RequestInfo | URL,
+  init?: RequestInit
+): Promise<Response> => {
+  const res = await fetch(input, init)
+
+  if (!res.ok && res.status !== 404) {
+    let errorMessage = `An API error occurred: ${res.status} ${res.statusText}`;
+    try {
+      const errorData = await res.json();
+      if (errorData.message) {
+        errorMessage = errorData.message;
+      }
+    } catch (e) {
+        console.log(e);
+    }
+    throw new Error(errorMessage);
+  }
+
+  return res
+}
+
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!, {
+  fetch: customFetch,
+})
